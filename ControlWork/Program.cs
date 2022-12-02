5a843dc39ea850e0d540a2e02af400e939db4c5f
Nina Khmissi <samninvik@mail.ru>
2022-12-03 02:10:11 +0500

    заполнение массива строками

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -1,4 +1,15 @@
-﻿Console.Clear();
+﻿string [] StringArray(string[]array)
+{
+for (int i=0; i<array.Length;i++)
+{
+    Console.WriteLine($"Введите {i+1}-ую строку:");
+    array[i]=Console.ReadLine();
+}
+Console.WriteLine();
+return array;
+}
+
+Console.Clear();
 
 Console.WriteLine("Введите количество строк в создаваемом массиве:");
 int number=int.Parse(Console.ReadLine());
