a34af3d6bd9defff6c92c343e96f01fb224760f0
Nina Khmissi <samninvik@mail.ru>
2022-12-03 02:12:43 +0500

    создпгтн метода отбора строк по длине

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -18,6 +18,22 @@ void PrintArray(string [] array)
      }
     Console.WriteLine();
 }
+
+string [] NewStringArray (string[]array)
+{
+	string [] newArray = new string [array.Length];
+	int k=0;
+	for (int i=0; i<array.Length;i++)
+{
+	if (array[i].Length<=3)
+	{
+		newArray[k]=array[i];
+		k++;
+	}
+
+}
+}
+
 Console.Clear();
 
 Console.WriteLine("Введите количество строк в создаваемом массиве:");
